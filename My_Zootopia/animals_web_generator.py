95b767f543601793dd31357aa70168a701547481
Erick-MG <erick0p_@hotmail.com>
2025-03-04 15:23:39 +0100

    access data from json file

--- My_Zootopia/animals_web_generator.py
+++ My_Zootopia/animals_web_generator.py
@@ -5,4 +5,27 @@ def load_data(file_path):
   with open(file_path, "r") as handle:
     return json.load(handle)
 
-animals_data = load_data('animals_data.json')
+
+# crear una function para  for loop con toda la info
+def user_input_animal(animal_data):
+  for animal_dict in animal_data:
+    name = animal_dict["name"]
+    diet = animal_dict["characteristics"]["diet"]
+    locations = animal_dict["locations"][0]
+
+
+    print(f"Name: {name}")
+    print(f"Diet: {diet}")
+    print(f"Location: {locations}")
+    if "type" in animal_dict["characteristics"]:
+      print(f"Type: {animal_dict["characteristics"]["type"]}")
+
+
+def main():
+    animals_data = load_data('animals_data.json')
+    print(animals_data)
+    user_input_animal(animals_data)
+
+
+if __name__ == "__main__":
+  main()
