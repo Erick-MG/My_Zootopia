5cacb440636c258337cbf46d2a716428d6ea81bb
Erick-MG <erick0p_@hotmail.com>
2025-03-13 13:48:44 +0100

    bring animal data into html

--- My_Zootopia/animals_web_generator.py
+++ My_Zootopia/animals_web_generator.py
@@ -5,27 +5,35 @@ def load_data(file_path):
   with open(file_path, "r") as handle:
     return json.load(handle)
 
+def load_html(animals_template):
+  with open(animals_template, "r") as file:
+    return file.read()
 
-# crear una function para  for loop con toda la info
+
 def user_input_animal(animal_data):
-  for animal_dict in animal_data:
-    name = animal_dict["name"]
-    diet = animal_dict["characteristics"]["diet"]
-    locations = animal_dict["locations"][0]
 
+    output = ''
+    for animal_dict in animal_data:
+        output += f"Name: {animal_dict["name"]}\n"
+        output += f"Diet: {animal_dict["characteristics"]["diet"]}\n"
+        output += f"Location: {animal_dict["locations"][0]}\n"
+        if "type" in animal_dict["characteristics"]:
+            output += f"Type: {animal_dict["characteristics"]["type"]}\n"
+    return output
 
-    print(f"Name: {name}")
-    print(f"Diet: {diet}")
-    print(f"Location: {locations}")
-    if "type" in animal_dict["characteristics"]:
-      print(f"Type: {animal_dict["characteristics"]["type"]}")
+def write_html(replace_html):
+    with open('animals.html', "w") as file:
+        return file.write(replace_html)
 
 
 def main():
     animals_data = load_data('animals_data.json')
     print(animals_data)
     user_input_animal(animals_data)
-
+    html_data = load_html("animals_template.html")
+    output = user_input_animal(animals_data)
+    replace_html =  html_data.replace("__REPLACE_ANIMALS_INFO__", output)
+    write_html(replace_html)
 
 if __name__ == "__main__":
   main()
