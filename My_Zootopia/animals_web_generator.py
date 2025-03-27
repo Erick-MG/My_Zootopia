146d044c3aab740455c74d8e7ad5864fcb56ccc1
Erick-MG <erick0p_@hotmail.com>
2025-03-27 10:18:06 +0100

    improve card item html

--- My_Zootopia/animals_web_generator.py
+++ My_Zootopia/animals_web_generator.py
@@ -1,41 +1,38 @@
 import json
 
 def load_data(file_path):
-  """ Loads a JSON file """
-  with open(file_path, "r") as handle:
-    return json.load(handle)
+    with open(file_path, "r") as handle:
+        return json.load(handle)
 
-def load_html(animals_template):
-  with open(animals_template, "r") as file:
-    return file.read()
-
+def load_html(template_path):
+    with open(template_path, "r") as file:
+        return file.read()
 
 def user_input_animal(animal_data):
-
     output = ''
     for animal_dict in animal_data:
-        output += '<li class="cards__item">'
-        output += f"Name: {animal_dict["name"]}<br/>\n"
-        output += f"Diet: {animal_dict["characteristics"]["diet"]}<br/>\n"
-        output += f"Location: {animal_dict["locations"][0]}\n"
+        output += '<li class="cards__item">\n'
+        output += f"<div class='card__title'> {animal_dict['name']}</div>\n"
+        output += f"<strong>Diet:</strong> {animal_dict['characteristics']['diet']}<br/>\n"
+        output += f"<strong>Location:</strong> {animal_dict['locations'][0]}<br/>\n"
         if "type" in animal_dict["characteristics"]:
-            output += f"Type: {animal_dict["characteristics"]["type"]}\n"
-        output += '</li>'
+            output += f"<strong>Type:</strong> {animal_dict['characteristics']['type']}<br/>\n"
+        output += '</li>\n'
     return output
 
-def write_html(replace_html):
-    with open('animals.html', "w") as file:
-        return file.write(replace_html)
-
+def write_html(output_path, replace_html):
+    with open(output_path, "w") as file:
+        file.write(replace_html)
 
 def main():
-    animals_data = load_data('animals_data.json')
-    print(animals_data)
-    user_input_animal(animals_data)
-    html_data = load_html("animals_template.html")
-    output = user_input_animal(animals_data)
-    replace_html =  html_data.replace("__REPLACE_ANIMALS_INFO__", output)
-    write_html(replace_html)
+    animals_data = load_data("animals_data.json")
+    html_template = load_html("animals_template.html")
+    animal_html = user_input_animal(animals_data)
+
+    replace_html = html_template.replace("__REPLACE_ANIMALS_INFO__", animal_html)
+    write_html("animals.html", replace_html)
+
+    print("Animals.html has been successfully generated!")
 
 if __name__ == "__main__":
-  main()
+    main()
